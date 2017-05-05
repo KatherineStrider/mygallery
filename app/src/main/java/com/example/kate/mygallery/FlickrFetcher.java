225dbc473531677b67b31a764628f93502186547
KatherineStrider <kotopesska112@yandex.ru>
2017-05-05 18:19:40 +0400

    Составление запроса

--- app/src/main/java/com/example/kate/mygallery/FlickrFetcher.java
+++ app/src/main/java/com/example/kate/mygallery/FlickrFetcher.java
@@ -1,9 +1,12 @@
 package com.example.kate.mygallery;
 
 import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +22,8 @@ public class FlickrFetcher {
 
     private static final String TAG = GalleryActivity.class.getSimpleName();
     private static final String API_KEY = "";
+    private static final String ERROR_MESSAGE_DATA_LOAD_FAILED = "Ошибка загрузки данных";
+    private static final String ERROR_MESSAGE_JSON = "Ошибка парсинга Json";
 
     public String getJSONString(String urlSpec) throws IOException{
 
@@ -39,6 +44,22 @@ public class FlickrFetcher {
 
         try{
             String url = Uri.parse("https://api.flickr.com/services/rest").buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+
+            String jsonString = getJSONString(url);
+            JSONObject jsonBody = new JSONObject(jsonString);
+        } catch (IOException e) {
+
+            Log.e(TAG, ERROR_MESSAGE_DATA_LOAD_FAILED, e);
+        }catch (JSONException e) {
+            Log.e(TAG, ERROR_MESSAGE_JSON, e);
         }
+
+        return galleryViews;
     }
 }
