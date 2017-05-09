11fab4557627173f58b5b4cb36405e2030be7b9a
KatherineStrider <kotopesska112@yandex.ru>
2017-05-09 16:52:42 +0400

    Добавление AsyncTask и установка адаптера для RecyclerView

--- app/src/main/java/com/example/kate/mygallery/GalleryActivity.java
+++ app/src/main/java/com/example/kate/mygallery/GalleryActivity.java
@@ -1,9 +1,12 @@
 package com.example.kate.mygallery;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.example.kate.mygallery.adapters.PhotosAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,5 +24,27 @@ public class GalleryActivity extends AppCompatActivity {
 
         photosView = (RecyclerView) findViewById(R.id.recycler_view_for_photos);
         photosView.setLayoutManager(new GridLayoutManager(this, 3));
+
+        new FetchItemTask().execute();
+
+        setupAdapter();
+    }
+
+    private void setupAdapter(){
+        photosView.setAdapter(new PhotosAdapter(galleryViews));
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryView>>{
+
+        @Override
+        protected List<GalleryView> doInBackground(Void... params) {
+            return new FlickrFetcher().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryView> items) {
+            galleryViews = items;
+            setupAdapter();
+        }
     }
 }
